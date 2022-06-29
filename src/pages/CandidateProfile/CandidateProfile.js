52aa6ae3136853c463e70b4c1d85c109451c4b7d
Gabriele Piaggesi <piaggesigabriele@gmail.com>
2022-06-29 13:05:25 +0200

    #1 - starting be

--- src/pages/CandidateProfile/CandidateProfile.js
+++ src/pages/CandidateProfile/CandidateProfile.js
@@ -6,8 +6,16 @@ import Card from '../../UI/Card/Card';
 const CandidateProfile = (props) => {
     return (
         <div className={`${classes.CandidateProfile} flex fRow w80 mAuto gap20`}>
-            <Card className="pad20 w20 hFitCont">
-                <p>Mario Rossi</p>
+            <Card className="pad20 w20 hFitCont flex fColumn fCenter gap15">
+                <div className="q150 divImage" style={{backgroundImage: `url(${'../companylogo.webp'})`}}></div>
+                <p className="pL">Mario Rossi</p>
+                <div className="flex fColumn fCenter gap5">
+                    <p className="pM">Maschio, 25 Anni</p>
+                    <p className="pM">Nato il 28/02/1997</p>
+                    <p className="pM">piaggesigabriele@gmail.com</p>
+                    <p className="pM">+393884661682</p>
+                </div>
+                <p className="pXL">8.5</p>
             </Card>
             <Card className="pad20 w80 hFCon">
                 <p>Mario Rossi</p>  
